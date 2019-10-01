0cbf92d20148a8117b604c45fced6137bc008401
fabrice1618 <fabrice1618@gmail.com>
2019-10-01 05:23:56 +0200

    version 2

--- charge_prenom.php
+++ charge_prenom.php
@@ -5,14 +5,19 @@ define("DB_USER", "root");
 define("DB_PWD",  "");
 define("DB_BASE", "prenom2");
 
+echo date("Ymd H:i ")."Début\n";
+
 $dbLink = mysqli_connect(DB_HOST, DB_USER, DB_PWD) or die("Impossible de se connecter : " . mysqli_error());
 if (mysqli_select_db($dbLink, DB_BASE)) {
     truncateTable("prenom");
 
     loadPrenom();
+} else {
+    echo "Impossible de se connecter à " . DB_BASE . PHP_EOL;
 }
 
 mysqli_close($dbLink);
+echo date("Ymd H:i ")."Fin\n";
 
 
 // Functions
@@ -20,7 +25,7 @@ function loadPrenom()
 {
     global $dbLink;
 
-    echo "LoadPrenom\n";
+//    echo "LoadPrenom\n";
     $fp = fopen("dpt2017.txt", "r");
     while ( ($sLine = fgets($fp, 4096)) !== false) {
             //echo $sLine;
@@ -28,9 +33,12 @@ function loadPrenom()
 
             $sQueryTemplate = 'INSERT INTO prenom (`sexe`, `prenom`, `annee`, `dept`, `nombre`) VALUES ("%s", "%s", "%s", "%s", "%s")';
             $sQuery = sprintf($sQueryTemplate, $sSexe, $sPrenom, $sAnnee, $sDepart, intval($sNombre) );
-            echo $sQuery . "\n";
-            if ($dbLink->query($sQuery) === TRUE) {
-                printf("Requete OK\n");
+//            echo $sQuery . "\n";
+            if ($dbLink->query($sQuery) !== TRUE) {
+//                printf("Requete OK\n");
+//            }
+//            else {
+                printf("sql error: %s\nQuery=%s", mysqli_error($dbLink), $sQuery );
             }
     }
 
